67eed1ac03d26e5f19afc368d553bfa763fb83ad
cedric sambat <cedricsambat9818@gmail.com>
2022-11-21 20:57:00 +0400

    cards_added

---
+++ vendor/assets/javascripts/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/vendor/assets/javascripts/particlesjs-config.json', function() {
+  console.log('callback - particles-js config loaded');
+});
